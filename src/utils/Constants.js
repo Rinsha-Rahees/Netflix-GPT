0c1fb9776cc2029cf38bbd227c477797de27f908
RinshaRahees <104898673+RinshaRahees@users.noreply.github.com>
2024-03-30 12:16:41 +0530

    Multi Lingual Support Add: Select box for language switch Feat: Spanish, Japanese

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,13 +1,25 @@
-export const LOGO = "https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.png"
-export const LOGIN_BG = "https://assets.nflxext.com/ffe/siteui/vlv3/9d3533b2-0e2b-40b2-95e0-ecd7979cc88b/a3873901-5b7c-46eb-b9fa-12fea5197bd3/IN-en-20240311-popsignuptwoweeks-perspective_alpha_website_small.jpg"
-export const USER_AVATAR = "https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.webp"
+export const LOGO =
+  "https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.png";
+export const BG_POSTER =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/9d3533b2-0e2b-40b2-95e0-ecd7979cc88b/a3873901-5b7c-46eb-b9fa-12fea5197bd3/IN-en-20240311-popsignuptwoweeks-perspective_alpha_website_small.jpg";
+export const USER_AVATAR =
+  "https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.webp";
 
 export const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzY2NjNjI5MTcxNGEwNmE1MjA0MzY5MTg4ZjA5MWUwMiIsInN1YiI6IjY0NzViY2QzZGQyNTg5MDBjMzI1MTc1YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.646E3IBsSnyuyEwGAQr9UUpc5dzx2461LXyone6prxE'
-    }
-  };
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer " + process.env.REACT_APP_TMDB_KEY,
+  },
+};
 
-export const TMDB_IMAGE_CDN_URL = "https://image.tmdb.org/t/p/w500"
+export const TMDB_IMAGE_CDN_URL = "https://image.tmdb.org/t/p/w500";
+
+export const SUPPORTED_LANGUAGES = [
+  { identifier: "en", name: "English" },
+  { identifier: "spanish", name: "Spanish" },
+  { identifier: "japanese", name: "Japanese" }
+];
+
+export const OPENAI_API_KEY = process.env.REACT_APP_OPENAI_API_KEY
---
+++ src/utils/LanguageConstants.js
@@ -0,0 +1,60 @@
+const lang = {
+
+    en: {
+        name: "English",
+        askGpt: "Ask GPT",
+        home: "Home",
+        signOut: "Sign Out",
+
+        play: "Play",
+        moreInfo: "More Info",
+
+        nowPlaying: "Now Playing",
+        trendingMovies: "Trending Movies",
+        popularMovies: "Popular Movies",
+        upcomingMovies: "Upcoming Movies",
+
+        search: "Search",
+        gptSearchPlaceholder: "What would you lik to watch today?",
+  
+    },
+    spanish: {
+        name: "Spanish",
+        askGpt: "Pregúntale a GPT",
+        home: "página principal",
+        signOut: "desconectar",
+
+        play: "Jugar",
+        moreInfo: "Más información",
+
+        nowPlaying: "Jugando ahora",
+        trendingMovies: "Películas de moda",
+        popularMovies: "Películas populares",
+        upcomingMovies: "Próximas películas",
+
+        search: "Buscar",
+        gptSearchPlaceholder: "¿Qué te gustaría ver hoy?",
+
+    },
+    japanese: {
+        name: "Japanese",
+        askGpt: "GPTに問い合わせる",
+        home: "ホームページ",
+        signOut: "サインアウト",
+
+        play: "遊ぶ",
+        moreInfo: "より詳しい情報",
+
+        nowPlaying: "再生中",
+        trendingMovies: "話題の映画",
+        popularMovies: "人気の映画",
+        upcomingMovies: "今後の映画",
+
+        search: "検索",
+        gptSearchPlaceholder: "今日は何を見たいですか？",
+
+    }
+
+}
+
+export default lang;
